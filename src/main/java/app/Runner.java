70b5aaf00734e58ba0ad8cfcd963410947285cda
Ahmet Kucuk <ahmetkucuk92@gmail.com>
2015-11-20 17:37:14 -0500

    start args are logged

--- src/main/java/app/Runner.java
+++ src/main/java/app/Runner.java
@@ -32,10 +32,11 @@ public class Runner {
     static String[] arg = new String[] {EVENT_INPUT_FILE, FINAL_DATA_IMAGE_OUTPUT, "100", "10000"};
     public static void main(String[] args) throws Exception {
 
+        System.out.println("Start with args: \t" + args[0] + "\t" + args[1] + "\t" + args[2] + "\t" + args[3]);
         long startTime = System.currentTimeMillis();
         new JP2DownloaderService().downloadImageFromFile(args[0], args[1], Integer.parseInt(args[2]), Integer.parseInt(args[3]));
         long endTime = System.currentTimeMillis() - startTime;
-        System.out.println("Duration: " + (endTime/ (1000 * 60)));
+        System.out.println("End. Duration: " + (endTime/ (1000 * 60)));
     }
 
     public static void downloadFinalImages() {
