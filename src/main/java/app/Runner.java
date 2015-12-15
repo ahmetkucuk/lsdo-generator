8ed432fd40701bfa175aba8472730e4a00e91e11
Ahmet Kucuk <ahmetkucuk92@gmail.com>
2015-12-15 02:04:08 -0500

    decide if server or local

--- src/main/java/app/Runner.java
+++ src/main/java/app/Runner.java
@@ -11,14 +11,29 @@ public class Runner {
     public static final String EVENT_SECONDARY_INPUT_FILE = "/Users/ahmetkucuk/Documents/Research/DNNProject/Final_Data/events_secondary.txt";
     public static final String FINAL_DATA_IMAGE_OUTPUT = "/Users/ahmetkucuk/Documents/Research/DNNProject/Final_Data/images/";
 
-    static String[] args = new String[] {EVENT_SECONDARY_INPUT_FILE, FINAL_DATA_IMAGE_OUTPUT, "10", "0"};
-    public static void main(String[] arg) throws Exception {
+    static String[] localArguments = new String[] {EVENT_SECONDARY_INPUT_FILE, FINAL_DATA_IMAGE_OUTPUT, "10", "0"};
+
+    public static boolean isLocal = true;
+
+    public static void main(String[] args) throws Exception {
+
+        isLocal = isMac(System.getProperty("os.name").toLowerCase());
+
+        if(isLocal) {
+            args = localArguments;
+        }
 
         System.out.println("Start with args: \t" + args[0] + "\t" + args[1] + "\t" + args[2] + "\t" + args[3]);
         long startTime = System.currentTimeMillis();
         new JP2DownloaderService().downloadImageFromFile(args[0], args[1], Integer.parseInt(args[2]), Integer.parseInt(args[3]), 0);
         long endTime = System.currentTimeMillis() - startTime;
         System.out.println("End. Duration: " + (endTime/ (1000 * 60)));
+    }
+
+    public static boolean isMac(String osName) {
+
+        return (osName.indexOf("mac") >= 0);
+
     }
 
     public static void downloadFinalImages() {
